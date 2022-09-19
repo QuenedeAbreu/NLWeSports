a805fd880ecd003d6ced59b6e5154fb5837d0666
Quenede Abreu <quenede.in@gmail.com>
2022-09-19 17:52:19 -0500

    last update

--- frontEndMobile/src/components/GameCard/index.tsx
+++ frontEndMobile/src/components/GameCard/index.tsx
@@ -1,13 +1,13 @@
-import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, Text } from 'react-native';
 
 import { styles } from './styles';
 import { LinearGradient } from 'expo-linear-gradient';
 import { THEME } from '../../theme';
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: { ads: number };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -20,7 +20,7 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
 
         <LinearGradient
@@ -28,10 +28,10 @@ export function GameCard({ data, ...rest }: Props) {
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
           <Text style={styles.ads}>
-            {data.ads} anuncios
+            {data._count.ads} anuncios
           </Text>
         </LinearGradient>
       </ImageBackground>
